a0433536ef4fd6db080ada02afda6633b6cab620
Brandon Marzolf <bmarzolf@globusmedical>
2018-02-20 16:41:20 -0500

    initial commit

---
+++ Append Suffix.py
@@ -0,0 +1,52 @@
+#! /usr/bin/python
+
+import os
+
+ignoreList = ["title_names.py", "mm"]
+exitList = ["exit", "EXIT", "done", "DONE", "n", "no", "N", "NO","Quit","quit","Q","q"]
+helpList = ["-h","--help","help","man","h"]
+
+helpText = """
+--------------------------------------------------------
+Enter a complete file path into the directory of which
+you want the contents to be appended with a suffix.
+You will then be asked for the suffix text to append.
+Suffixes are appended before the extension.
+
+Append Suffix is not recursive.
+Sub directories will be renamed but not their contents.
+--------------------------------------------------------
+
+"""
+
+suffix = ""
+
+def getDirLoc():
+    global suffix
+    dirLoc = input("This will add a suffix to all files in a directory\n Directory?\n")
+    if dirLoc in helpList:
+        print(helpText)
+        return getDirLoc()
+    if dirLoc in exitList:
+        exit(0)
+    suffix = input("Suffix: ")
+    return dirLoc
+
+location = getDirLoc()
+
+while True:
+    location += "\\"
+
+    for file in os.listdir(location):
+        newName = file
+        dotIdx = file.rfind(".")
+        newName = file[:dotIdx] + suffix + file[dotIdx:]
+        print(newName)
+        file = location + file
+        newName = location + newName
+        print(newName)
+        os.rename(file, newName)
+
+    location = getDirLoc()
+
+exit(0)
---
+++ Title_File_Names.py
@@ -0,0 +1,58 @@
+#! /usr/bin/python
+
+import os
+
+ignoreList = ["title_names.py", "mm"]
+exitList = ["exit", "EXIT", "done", "DONE", "n", "no", "N", "NO","Quit","quit","Q","q"]
+helpList = ["-h","--help","help","man","h"]
+
+helpText = """
+-------------------------------------------------------
+Enter a complete file path into the directory of which
+you want the contents to be renamed in 'title' format.
+File extensions will be ignored.
+
+Title File Names is not recursive.
+Sub directories will be renamed but not their contents.
+-------------------------------------------------------
+
+"""
+
+def getDirLoc():
+    dirLoc = input("Which directory to rename?\n")
+    if dirLoc in helpList:
+        print(helpText)
+        return getDirLoc()
+    if dirLoc in exitList:
+        exit(0)
+    return dirLoc
+
+location = getDirLoc()
+
+while True:
+    location += "\\"
+
+    for file in os.listdir(location):
+        newName = file.lower()
+        wordList = newName.split(" ")
+        newName = ""
+        print(wordList)
+        for w in wordList:
+            if w not in ignoreList:
+                if w[0].isalpha():
+                    cList = list(w)
+                    cList[0] = cList[0].title()
+                    w = "".join(cList)
+                    newName += w + " "
+                else:
+                    newName += w + " "
+            else:
+                newName += w + " "
+        print("Renaming " + file + " to " + newName)
+        file = location + file
+        newName = location + newName
+        os.rename(file, newName)
+
+    location = getDirLoc()
+
+exit(0)
